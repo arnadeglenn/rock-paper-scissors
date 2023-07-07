2058b647b81c069f443aef49829009b02ae46115
Glenn Arnade <125632656+arnadeglenn@users.noreply.github.com>
2023-07-06 22:35:18 -0700

    Created rock,paper,scissors game via JS w/o score

---
+++ pseudocode.md
@@ -0,0 +1,26 @@
+COMPUTER CHOICE
+allow the computer to make a random choice of rock, paper, or scissors
+
+
+USER CHOICE
+Create variable based on button selection
+
+HUMAN vs COMPUTER
+
+Switch statement
+
+human chooses Rock
+if computer = rock, It's a Tie!
+else if computer = paper, You Lose
+else computer = scissors, You Win!
+
+human chooses Paper
+if computer = rock, You Win!
+else if computer = paper, It's a Tie!
+else computer = scissors, You Lose!
+
+human chooses Scissors
+if computer = rock, You Lose!
+else if computer = paper, You Win!
+else computer = scissors, It's a Tie!
+
--- script.js
+++ script.js
@@ -1,1 +1,77 @@
 console.log("hello,world!");
+
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random()*3)
+
+    if (compChoice === 0) {
+    return "Rock";
+    } else if (compChoice === 1) {
+        return "Paper";
+    } else if (compChoice === 2) {
+        return "Scissors";
+    }
+    }  
+
+document.addEventListener("DOMContentLoaded", function() {
+    let humanChoice = "";
+
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
+
+rockButton.addEventListener("click", function() {
+    humanChoice = "Rock";
+    compareChoice();
+});
+paperButton.addEventListener("click", function() {
+    humanChoice = "Paper";
+    compareChoice();
+});
+scissorsButton.addEventListener("click", function() {
+    humanChoice = "Scissors";
+    compareChoice();
+});
+
+function compareChoice(){
+    let compChoice = getComputerChoice();  
+switch (compChoice) {
+    case "Rock":
+      if (humanChoice === "Rock") {
+        console.log("It's a Tie!");
+      } else if (humanChoice === "Paper") {
+        console.log("You Lose!");
+      } else if (humanChoice === "Scissors") {
+        console.log("You Win!");
+      }
+      break;
+  
+    case "Paper":
+      if (humanChoice === "Rock") {
+        console.log("You Win!");
+      } else if (humanChoice === "Paper") {
+        console.log("It's a Tie!");
+      } else if (humanChoice === "Scissors") {
+        console.log("You Lose!");
+      }
+      break;
+  
+    case "Scissors":
+      if (humanChoice === "Rock") {
+        console.log("You Lose!");
+      } else if (humanChoice === "Paper") {
+        console.log("You Win!");
+      } else if (humanChoice === "Scissors") {
+        console.log("It's a Tie!");
+      }
+      break;
+  
+    default:
+      console.log("Invalid choice.");
+  }
+}  
+});
+
+
+
+
+
