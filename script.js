bd10cb3b4116e41935659f2bb289fc0632683f0a
Glenn Arnade <125632656+arnadeglenn@users.noreply.github.com>
2023-08-04 13:41:04 -0700

    Added a DOM button to play RPS

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 console.log("hello,world!");
 let humanScore = 0;
 let computerScore = 0;
+
+const rockButton = document.querySelector('#rockButton');
+const paperButton = document.querySelector('#paperButton');
+const scissorsButton = document.querySelector('#scissorsButton');
+const startOver = document.querySelector('#startOver');
 
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random()*3)
@@ -12,76 +19,73 @@ function getComputerChoice() {
     } else if (compChoice === 2) {
         return "scissors";
     }
-    }  
+    };  
 
-function getHumanChoice() {
-  const humanType = prompt('Type Rock, Paper, or Scissors',);
-  const lowercaseType = humanType.toLowerCase();
 
-  if (lowercaseType !== "rock" && lowercaseType !== "paper" && lowercaseType !== "scissors") {
-      console.log("You suck at typing");
-  } else {
-      return lowercaseType;
-  }
-}
+rockButton.addEventListener('click', function() {
+  playRound('rock');
+  });
+  
+paperButton.addEventListener('click', function() {
+  playRound('paper');
+ });
+  
+scissorsButton.addEventListener('click', function() {
+    playRound('scissors');
+   });
 
-function playRound(){
+function playRound(humanChoice){
     let compChoice = getComputerChoice(); 
-    let humanChoice =  getHumanChoice();
+    let result;
 switch (compChoice) {
     case "rock":
       if (humanChoice === "rock") {
-        return "It's a Tie!";
+        result = "It's a Tie!";
       } else if (humanChoice === "paper") {
-        return "You Lose!";
+        result = "You Lose!";
       } else if (humanChoice === "scissors") {
-        return "You Win!";
+        result = "You Win!";
       }
       break;
   
     case "paper":
       if (humanChoice === "rock") {
-        return "You Win!";
+        result = "You Win!";
       } else if (humanChoice === "paper") {
-        return "It's a Tie!";
+        result = "It's a Tie!";
       } else if (humanChoice === "scissors") {
-        return "You Lose!";
+        result = "You Lose!";
       }
       break;
   
     case "scissors":
       if (humanChoice === "rock") {
-        return "You Lose!";
+        result = "You Lose!";
       } else if (humanChoice === "paper") {
-        return "You Win!";
+        result = "You Win!";
       } else if (humanChoice === "scissors") {
-        return "It's a Tie!";
+        result = "It's a Tie!";
       }
       break;
   
     default:
       console.log("Invalid choice.");
   }
+  console.log(result);
+  scoreboard(result);
+  console.log(humanScore + "-" + computerScore);
+  announceWinnerRestart();
 }  
 
-function game() {
-  for (let i=0; i<6; i++) {
-    let result = playRound();
+function scoreboard(result) {
     if (result === "You Win!") {
       humanScore++;
     } else if (result === "You Lose!") {
       computerScore++;
-    } else if (result === "It's a Tie!") {
-    } 
-  
-    console.log(result);
-    console.log(humanScore + "-" + computerScore);
-    announceWinner();
-  }
+    };
+  };
 
-}
-
-function announceWinner() {
+function announceWinnerRestart() {
   if (humanScore === 5) {
     console.log("YESSUH BLESSUH YOU BEAT THAT BITCH!")
     humanScore = 0;
@@ -93,8 +97,15 @@ function announceWinner() {
   }
 };
 
-game();
 
+
+
+
+
+
+
+
+});
 
 
 
